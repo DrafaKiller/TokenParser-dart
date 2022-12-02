2ed3acd2507277ebf6b13c0cdcc929c75da79aa9
Diogo Silva <drafa1999@gmail.com>
2022-12-02 17:08:55 +0000

    Translate Pattern to Lexeme when possible

--- lib/src/lexemes/abstracts/parent.dart
+++ lib/src/lexemes/abstracts/parent.dart
@@ -1,8 +1,13 @@
 import 'package:token_parser/src/lexeme.dart';
+import 'package:token_parser/src/lexemes/pattern.dart';
+import 'package:token_parser/utils/type.dart';
 
 abstract class ParentLexeme<PatternT extends Pattern> extends Lexeme {
   @override
   final List<PatternT> children;
 
-  ParentLexeme(this.children, { super.name, super.grammar });
+  ParentLexeme(List<Pattern> children, { super.name, super.grammar }) :
+    children = isSubType<PatternT, Lexeme>()
+      ? children.map((child) => PatternLexeme(child) as PatternT).toList()
+      : children.cast<PatternT>();
 }
